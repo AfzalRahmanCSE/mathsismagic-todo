a15c3a15c638477db6f3201d92ff1ed1a3b021a8
AfzalUrRahman <afzalrahman.cse@gmail.com>
2023-04-18 15:29:14 +0530

    jest config file changed

--- jest.config.js
+++ jest.config.js
@@ -159,12 +159,12 @@ module.exports = {
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: [
+    "\\\\node_modules\\\\"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  // testRegex: [],
+  testRegex: [],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
